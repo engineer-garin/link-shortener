47645a973ec3b5b0025cf9730428909e8d55fd8a
garin <nope@nope>
2018-02-16 05:36:31 -0800

    updated orm

--- link_shortener/orm.py
+++ link_shortener/orm.py
@@ -1,5 +1,6 @@
 
 import boto3
+import validators
 
 class URLShortenerMappingFactory(object):
     def __init__(self, region_name, dynamodb_table_name):
@@ -7,15 +8,26 @@ class URLShortenerMappingFactory(object):
         self.dynamodb_table = dynamodb.Table(dynamodb_table_name)
 
     def from_id_num(id_num):
-        # TODO -- use id_num to look up URL in dynamodb
-        # TODO -- error out if no URL found
-        pass
+        response = table.get_item(
+            Key={'id_num': id_num}
+            ,ProjectionExpression='url'
+        )
+        if 'Item' in response:
+            return UrlShortenerMapping(id_num, response['Item']['url'])
+        else:
+            return None
 
     def from_url(url):
-        # TODO -- verify URL is correctly formatted HTTP/S URL 
-        # TODO -- verify URL is < 8 KB (limit for IE is 2 KB, higher for other browsers. No limit in RFC, although there is a limit for sitemap XML.)
-        # TODO -- upsert URL to get id_num
-        return URLShortenerMapping(id_num, url)
+        # verifies URL is < 8 KB
+        # limit for IE is 2 KB, higher for other browsers. No limit in RFC, although there is a limit for sitemap XML.
+        8k = 8 * 2 ** 10
+        if (len(url) < 8k and 
+            and (url.startswith('http://') or url.startswith('https://'))
+            and validators.url.url(url)):
+            # TODO -- upsert URL to get id_num
+            return URLShortenerMapping(id_num, url)
+        else:
+            raise Exception('url is improperly formatted')
 
 class URLShortenerMapping(object):
     def __init__(id_num, url):
